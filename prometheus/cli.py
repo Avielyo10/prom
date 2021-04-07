3c50c3f48c95fb3bb352d9c97da4ecc44b957026
Aviel Yosef <ayosef@redhat.com>
2021-04-07 15:46:26 +0300

    Implement head & tail

--- prometheus/cli.py
+++ prometheus/cli.py
@@ -49,7 +49,8 @@ Prometheus host, try
 @click.option('--output', '-o', type=click.Choice(['csv', 'json', 'yaml']), default='csv')
 @click.option('--sort-by', '-s', type=click.Choice(['min', 'max', 'avg']), default=None)
 @click.option('--metric-type', '-m', type=click.Choice(['housekeeping', 'infra']), default=None)
-def metrics(host, token, interval, time, skip_namespaces, output, sort_by, metric_type):
+@click.option('--tail/--head', default=None)
+def metrics(host, token, interval, time, skip_namespaces, output, sort_by, metric_type, tail):
     prometheus = Prometheus(host, token)
 
     metrics = {
@@ -74,6 +75,9 @@ def metrics(host, token, interval, time, skip_namespaces, output, sort_by, metri
 
     if sort_by is not None:
         df.sort_values(by=f'{sort_by} over {interval}', inplace=True)
+
+    if tail is not None:
+        df = df.tail() if tail else df.head()
 
     if output == 'json':
         df.to_json(sys.stdout, orient='index')
